f4474461ca7c0c8d6c7f05289b5b7920e60a5490
Kerri Sexton <k.sexton804@gmail.com>
2020-09-19 21:35:47 -0400

    wrote if statements to create a string from the user's choices

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A random password generator based on the criteria you select
 
 1. Created a password object with blank values.
 2. Working prompts that fill in the values for the object.
+3. If statements that create a string with the values the user wanted.
--- script.js
+++ script.js
@@ -3,8 +3,8 @@
       //lowercase, numeric and special chars
 //2. At least one char type should be selected
 //3. All prompt answer should be stored in an object
-//4. Create a new array of all chars chosen.
-//5. Select random chars from that array for however long the
+//4. Create a new String of all chars chosen.
+//5. Select random chars from that String for however long the
     //password should be
 //6. Password is now generated.
 //7. Return password to the user. (alert or written to page)
@@ -14,7 +14,7 @@
 // var generateBtn = document.querySelector("#generate");
 
 //password object
-var password = {
+var passwordObj = {
   length: '',
   upper:  '',
   lower: '',
@@ -22,29 +22,56 @@ var password = {
   spec: ''
 }
 
-var passwordArr = [];
-console.log('PasswordArr = ', passwordArr);
+//To be filled in later by user answers
+var passwordString = '';
+console.log('PasswordString = ', passwordString);
 
-password.length = prompt('How long would you like your password to be? (Must be between 8 and 128 characters)');
-console.log('Length is: ', password.length);
-while(password.length < 8 || password.length > 128){
-  password.length = prompt('How long would you like your password to be? (Must be between 8 and 128 characters)');
-  console.log('Length is: ', password.length);
+//Prompts to the user
+passwordObj.length = prompt('How long would you like your password to be? (Must be between 8 and 128 characters)');
+console.log('Length is: ', passwordObj.length);
+while(passwordObj.length < 8 || passwordObj.length > 128){
+  passwordObj.length = prompt('How long would you like your password to be? (Must be between 8 and 128 characters)');
+  console.log('Length is: ', passwordObj.length);
 }
 
+passwordObj.upper = prompt('Would you like uppercase letters in your password? (Press "ok" for yes)');
+console.log('Uppercase?: ', passwordObj.upper);
+
+passwordObj.lower = prompt('Would you like lowercase letters in your password? (Press "ok" for yes)');
+console.log('Lowercase?: ', passwordObj.lower);
+
+passwordObj.num = prompt('Would you like numerical values in your password? (Press "ok" for yes)');
+console.log('Numerical?: ', passwordObj.num);
+
+passwordObj.spec = prompt('Would you like special characters in your password? (Press "ok" for yes)');
+console.log('Special?: ', passwordObj.spec);
 
 
-password.upper = prompt('Would you like uppercase letters in your password? (Press "ok" for yes)');
-console.log('Uppercase?: ', password.upper);
+//Create a string with all the characters that the user wants.
+if(passwordObj.upper != null){
+  var upperString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  passwordString += upperString;
+  console.log('Full password string is: ', passwordString);
+}
 
-password.lower = prompt('Would you like lowercase letters in your password? (Press "ok" for yes)');
-console.log('Lowercase?: ', password.lower);
+if(passwordObj.lower != null){
+  var lowerString = 'abcdefghijklmnopqrstuvwxyz';
+  passwordString += lowerString;
+  console.log('Full password string is: ', passwordString);
+}
 
-password.num = prompt('Would you like numerical values in your password? (Press "ok" for yes)');
-console.log('Numerical?: ', password.num);
+if(passwordObj.num != null){
+  var numString = '0123456789';
+  passwordString += numString;
+  console.log('Full password string is: ', passwordString);
+}
 
-password.spec = prompt('Would you like special characters in your password? (Press "ok" for yes)');
-console.log('Special?: ', password.spec);
+if(passwordObj.spec != null){
+  var specString = '!@#$%^&*()-+=<>';
+  passwordString += specString;
+  console.log('Full password string is: ', passwordString);
+}
+
 
 
 
